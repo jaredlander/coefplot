dc7e4222c6381996bd61b56ec9c92f1f19c35689
Jared Lander <info@jaredlander.com>
2011-10-28 00:03:13 -0400

    Added arguments to multiplot

--- R/multiplot.r
+++ R/multiplot.r
@@ -15,15 +15,16 @@
 #' model1 <- lm(price ~ carat + cut, data=diamonds)
 #' model2 <- lm(price ~ carat + cut + color, data=diamonds)
 #' multiplot(model1, model2)
-multiplot <- function(...)
+multiplot <- function(..., outerCI=2, innerCI=1, intercept=TRUE, numeric=FALSE, sort="natural", decreasing=TRUE)
 {
     # grab the models
     theDots <- list(...)
     
     # need to add arguments for buildModelCI
     # functionize modelMelt
+    # need to change getModelInfo and buildModelCI and coefplot.lm so that shorten, factors and only are normal arguments and not part of ..., that way it will work better for this
     # get the modelCI for each model and make one big data.frame
-    modelCI <- ldply(theDots, .fun=buildModelCI)
+    modelCI <- ldply(theDots, .fun=buildModelCI, outerCI=outerCI, innerCI=innerCI, intercept=intercept, numeric=numeric, sort=sort, decreasing=decreasing)
     
     # Turn the Call into a unique identifier for each model
     modelCI$Call <- as.numeric(factor(modelCI$Call, levels=unique(modelCI$Call)))
