705083d910947982f2af55b6413099cc86bed73a
jaredlander <info@jaredlander.com>
2012-10-05 17:34:33 -0400

    Model info function for lmer models.

---
+++ R/lmer.r
@@ -0,0 +1,13 @@
+#' getModelInfo.mer
+#' 
+#' Title
+#' 
+#' Details
+#' @aliases getModelInfo.met
+#' @inheritParams getModelInfo
+#' @S3method getModelInfo mer
+#' @param effects Indicates if we are plotting fixed (individual level) or random (group) effects
+getModelInfo.mer <- function(model, effects=c("fixed", "random"), ...)
+{
+    return("hi")
+}
