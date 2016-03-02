b70686cca076616d1ea27d5e066e832a272ca1a5
Jared Lander <info@jaredlander.com>
2016-03-01 22:29:47 -0500

    Showing example of using invlogit for glm.

--- R/coefplot.r
+++ R/coefplot.r
@@ -279,6 +279,7 @@ coefplot.lm <- function(...)
 #' 
 #' model2 <- glm(price > 10000 ~ carat + cut*color, data=diamonds, family=binomial(link="logit"))
 #' coefplot(model2)
+#' coefplot(model2, trans=invlogit)
 coefplot.glm <- function(...)
 {
     coefplot.default(...)
