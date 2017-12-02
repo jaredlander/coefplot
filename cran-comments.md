0d6faa59116182ce00d255b380a4b29aee9140e1
Jared Lander <info@jaredlander.com>
2017-12-01 23:16:25 -0500

    Added tests for buildModelCI.
    More notes to CRAN.
    
    Added vignette infrastructure.

--- cran-comments.md
+++ cran-comments.md
@@ -3,11 +3,17 @@
 
 ## Test environments
 - travis-ci latest version
+    - R-oldrel
+    - R-release
+    - R-devel
 - Windows 10, R 3.4.2
 - Windows 10, R 3.5.0 (R-Devel)
 - Win Builder
 
 ## R CMD check results
-There were no ERRORs
+There were no ERRORs or WARNINGs other than a note to CRAN maintainers as seen below.
 
-On my PC, with R 3.4.2, I get the NOTE that some examples took over 5s but I do not see this note with other versions or on Win-Builder.
+* checking CRAN incoming feasibility ... Note_to_CRAN_maintainers
+Maintainer: 'Jared P. Lander <packages@jaredlander.com>'
+
+On my PC, with R 3.4.2, I get the NOTE that some examples took over 5s but I do not see this note with other versions. Same with Win-Builder but with fewer examples.
--- tests/testthat/test-buildModelCI.R
+++ tests/testthat/test-buildModelCI.R
@@ -1,5 +1,8 @@
 context("buildModelCI builds the appropriate model")
 
+library(glmnet)
+library(xgboost)
+
 data(diamonds, package='ggplot2')
 
 model1 <- lm(price ~ carat + cut, data=diamonds)
@@ -22,6 +25,41 @@ xgCI_named <- coefplot:::buildModelCI(xg1, feature_names=colnames(diaX))
 modG1 <- glmnet(x=diaX, y=diaY)
 coefG1 <- coefplot:::buildModelCI(modG1)
 
-test_that('', {
+correctNames <- c('Value', 'Coefficient', 'HighInner', 'LowInner', 'HighOuter', 'LowOuter', 'Model')
+
+test_that('buildModelCI returns a data.frame', {
+    expect_is(ci1, 'data.frame')
+    expect_is(xgCI, 'tbl')
+    expect_is(xgCI_named, 'tbl')
+    expect_is(coefG1, 'data.frame')
+})
+
+test_that('buildModelCI returns correct number and names of columns', {
+    expect_length(ci1, 7)
+    expect_length(xgCI, 7)
+    expect_length(xgCI_named, 7)
+    expect_length(coefG1, 7)
     
+    expect_named(ci1)
+    expect_named(xgCI)
+    expect_named(xgCI_named)
+    expect_named(coefG1)
+    
+    expect_equal(names(ci1), correctNames)
+    expect_equal(names(xgCI), correctNames)
+    expect_equal(names(xgCI_named), correctNames)
+    expect_equal(names(coefG1), correctNames)
+})
+
+
+test_that('buildModelCI returns correct number of rows', {
+    expect_equal(nrow(ci1), 6)
+    expect_equal(nrow(xgCI), 8)
+    expect_equal(nrow(xgCI_named), 8)
+    expect_equal(nrow(coefG1), 5)
+})
+
+
+test_that('buildModelCI gives warning for glmnet models', {
+    expect_warning(coefG1 <- coefplot:::buildModelCI(modG1))
 })
